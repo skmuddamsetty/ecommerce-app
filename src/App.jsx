dceb9b95844ec15b7ba634df0ae5d91fc76893b7
ssankathali <muddamsetty.santhosh@gmail.com>
2021-08-12 00:20:58 -0400

    basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Dashboard from "./Dashboard";
 import Login from "./Login";
+import Register from "./Register";
+import { HashRouter } from "react-router-dom";
+import { Switch, Route } from "react-router";
+import NoMatchPage from "./NoMatchPage";
 
 function App() {
-  return <Login />;
+  return (
+    <HashRouter>
+      <div className="container-fluid">
+        <Switch>
+          <Route path="/" exact={true} component={Login} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/register" component={Register} />
+          <Route path="*" component={NoMatchPage} />
+        </Switch>
+      </div>
+    </HashRouter>
+  );
 }
 export default App;
---
+++ src/NoMatchPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NoMatchPage(props) {
+  return <div>Page Not Found</div>;
+}
+
+export default NoMatchPage;
